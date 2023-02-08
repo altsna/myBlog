2539afe0d2b180c177113c0b406d22dddd586f6f
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-08 15:45:15 +0530

    add space

--- components/CommandPalette.js
+++ components/CommandPalette.js
@@ -17,6 +17,7 @@ export default function CommandPalette({ navigation }) {
         setIsOpen(!isOpen)
       }
     }
+
     window.addEventListener('keydown', handleKeyDown)
 
     return () => window.removeEventListener('keydown', handleKeyDown)
